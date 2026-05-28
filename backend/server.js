0dc5f878fc1cefd233317c0d7dae6b22d6e27e15
poornima-creator <poornikamalkathiresan@gmail.com>
2026-05-28 21:43:19 +0530

    updated frontend url

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,8 @@ const httpServer = http.createServer(app);
 
 const allowedOrigins = [
   'http://localhost:5173',          // local dev
-  process.env.CLIENT_URL            // production Vercel URL (set in env)
+  process.env.CLIENT_URL,
+  'https://ev-battery-monitor-ds4ziompi-poornima-k-s-projects.vercel.app'            // production Vercel URL (set in env)
 ];
 
 const io = new Server(httpServer, {
